abe50b69af8aa399ff599a04f30c060de0339edd
nguyenxuanvinh55th2 <nguyenxuanvinh55th2@gmail.com>
2016-05-07 21:59:49 +0700

    Update maincomponent.js

--- imports/ui/components/maincomponent/maincomponent.js
+++ imports/ui/components/maincomponent/maincomponent.js
@@ -1,14 +1,17 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
-import {name as Home} from '../home/home';
-import {name as Register} from '../register/register';
-import {name as RegisterStudent} from '../register_student/registerStudent';
-import {name as RegisterTeacher} from '../register_teacher/registerTeacher';
-import {name as Navigation} from '../navigation/navigation';
-import {name as Authenticated} from '../authenticated/authenticated';
-import {name as AddTest} from '../addtest/addtest';
 import uiRouter from 'angular-ui-router';
 import ngMaterial from 'angular-material';
+
+import { name as Home } from '../home/home';
+import { name as Register } from '../register/register';
+import { name as RegisterStudent } from '../register_student/registerStudent';
+import { name as RegisterTeacher } from '../register_teacher/registerTeacher';
+import { name as Navigation } from '../navigation/navigation';
+import { name as Authenticated } from '../authenticated/authenticated';
+import { name as AddTest } from '../addtest/addtest';
+import { name as ManageQuestion } from '../managequestion/managequestion';
+import { name as QuestionDetail } from '../questiondetail/questiondetail';
 
 import './maincomponent.html';
 
@@ -26,6 +29,8 @@ export default  angular.module(name, [
     RegisterStudent,
     Authenticated,
     AddTest,
+    ManageQuestion,
+    QuestionDetail
     ///ngMaterial
   ]
 )
